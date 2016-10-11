c99c7983026a25a42575988302b59bc8a0dd4ee6
RLRoux <roxane.rouxparrod@gmail.com>
2016-10-11 14:35:55 +0200

    Fix project structure and and unit tests pattern

--- src/fr/mrc/ptichat/test.java
+++
@@ -1,7 +0,0 @@
-package fr.mrc.ptichat;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/fr/mrc/ptichat/test/MainTest.java
@@ -0,0 +1,26 @@
+package fr.mrc.ptichat.test;
+
+import fr.mrc.ptichat.main.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by roxane on 11/10/2016.
+ */
+public class MainTest {
+    Main main;
+
+    @Before
+    public void init(){
+        System.out.println("Setting up ...");
+        this.main = new Main();
+    }
+    
+    @Test
+    public void testExample() throws Exception {
+        Assert.assertNotNull(main);
+        Assert.assertTrue(main.testExample());
+    }
+
+}
