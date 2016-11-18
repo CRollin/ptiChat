24628264f490854cc887898ca27ae0d09875e3e1
Charles Rollin <charles.rollin@student.ecp.fr>
2016-11-18 08:29:26 +0100

    Add test file to fileToByteArray unit test

--- src/main/java/fr/mrc/ptichat/test/ResponseHandlerTest.java
+++ src/main/java/fr/mrc/ptichat/test/ResponseHandlerTest.java
@@ -5,7 +5,10 @@ import main.java.fr.mrc.ptichat.processing.RequestHandler;
 import main.java.fr.mrc.ptichat.processing.ResponseHandler;
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ResponseHandlerTest {
 
@@ -25,8 +28,11 @@ public class ResponseHandlerTest {
     @Test
     public void fileToByteArray() {
         RequestHandler reqH = new RequestHandler();
-        byte[] toto = rh.fileToByteArray("./src/test.jpg");
-        reqH.byteArrayToFile("./src/test2.jpg", toto);
+        String pathToTesResources = "./src/main/java/fr/mrc/ptichat/test/resources";
+        byte[] toto = rh.fileToByteArray(pathToTesResources + "/test.jpg");
+        reqH.byteArrayToFile(pathToTesResources + "/test2.jpg", toto);
+        File newFile = new File(pathToTesResources + "/test2.jpg");
+        assertTrue(newFile.exists() && (System.currentTimeMillis() - newFile.lastModified()) <= 5000L);
     }
 
 }
