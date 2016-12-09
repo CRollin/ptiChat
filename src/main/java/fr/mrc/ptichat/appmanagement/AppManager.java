57a32b2339d34e0fc8ba6e9d50a0e9d650d9d041
RLRoux <roxane.rouxparrod@gmail.com>
2016-12-09 17:03:39 +0100

    Add a few comments and remove useless files

--- src/main/java/fr/mrc/ptichat/appmanagement/AppManager.java
+++ src/main/java/fr/mrc/ptichat/appmanagement/AppManager.java
@@ -5,6 +5,9 @@ import main.java.fr.mrc.ptichat.connection.ClientServer;
 import main.java.fr.mrc.ptichat.exceptions.NoServerException;
 import main.java.fr.mrc.ptichat.utils.LanguagesController;
 
+/**
+ * Coordinates the interactions between ui objects
+ */
 public class AppManager {
 
     private ConnectionManager connectionManager;
@@ -17,21 +20,36 @@ public class AppManager {
         this.chatManager = new ChatManager(this);
     }
 
+    /**
+     * Initializes the connection UI and open it
+     */
     public void init() {
         this.connectionManager.createUI();
         this.connectionManager.open();
     }
 
+    /**
+     * Closes the connection UI
+     * Initializes the chat UI and open it
+     */
     public void initChat(String peerIp) {
         this.connectionManager.dispose();
         this.chatManager.create(peerIp);
         this.chatManager.open();
     }
 
+    /*
+     * Reopens the connection UI when the user leaves the chat frame
+     */
     public void leaveChat() {
         this.connectionManager.open();
     }
 
+    /**
+     * Initializes connection between the peers after having completed the connection form.
+     * If the peer is not founded, the user wait for someone to connect on his port
+     * In this case, the identity of the peer that will connect to him is not checked
+     */
     public void initConnection(String peerIp, int peerPort, int userPort, String userName) {
         String dirPath = this.connectionManager.getSavedFilesDirectory();
         if (dirPath.length() > 0) this.chatManager.setSavedFilesDirectory(dirPath);
--- src/main/java/fr/mrc/ptichat/appmanagement/ConnectionManager.java
+++ src/main/java/fr/mrc/ptichat/appmanagement/ConnectionManager.java
@@ -7,6 +7,9 @@ import java.util.Hashtable;
 
 import static java.lang.Integer.parseInt;
 
+/**
+ * Manages the connection UI
+ */
 public class ConnectionManager {
     private AppManager am;
     private ConnectionUI cui;
